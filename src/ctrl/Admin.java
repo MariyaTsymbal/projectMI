c67e641f63c9e27919c4f51fb8ec6ee4aaad87a9
Ivona <iimm4@hotmail.com>
2016-12-02 00:30:43 -0500

    Admin uses prism accounts

--- src/ctrl/Admin.java
+++ src/ctrl/Admin.java
@@ -31,27 +31,72 @@ public class Admin extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-						
-		if (request.getSession().getAttribute("Authorized") != null){
-			//this.getServletContext().getRequestDispatcher("/Account.jsp").forward(request, response);
+	//doPost(request,response);
+	
+		if (request.getSession().getAttribute("Authorized") != null)
+	{
+		
+		if (request.getParameter("logout") !=null){
+			request.getSession().removeAttribute("Authorized");
+			request.getSession().invalidate();
+			this.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
 
-			if (request.getParameter("logout") !=null){
-				request.getSession().removeAttribute("Authorized");
-				this.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
-
+		}
+		else if(request.getParameter("browse") !=null){
+			this.getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+		}
+		else {
+		this.getServletContext().getRequestDispatcher("/Account.jsp").forward(request, response);
+		}
+	}
+	else
+	{
+		if (request.getParameter("login") != null)
+		{
+			String back= request.getRequestURL().toString();
+			String username=request.getParameter("name");
+			String password=request.getParameter("pass");
+			System.out.println(username+ " pass:"+password);
+			
+			String hash = "";
+			try
+			{
+				hash = javax.xml.bind.DatatypeConverter.printHexBinary(MessageDigest.getInstance("SHA1").digest(password.getBytes()));
+			} catch (NoSuchAlgorithmException e)
+			{
+				e.printStackTrace();
 			}
-			else if(request.getParameter("browse") !=null){
-				this.getServletContext().getRequestDispatcher("/Catalogue.jsp").forward(request, response);
-
-			}
-			else {
-				this.getServletContext().getRequestDispatcher("/Account.jsp").forward(request, response);
-
-			}
+			response.sendRedirect("https://"+username+":"+hash+
+						"@www.eecs.yorku.ca/~ivona95/auth/Auth.cgi?back="+back);
+			
+			
+			
+			//if(q.indexOf("user=")>0){
+			//username =q.substring(q.indexOf("user="));
+			//}
+			System.out.println(response.getHeader("user"));
+			System.out.println("What is back? "+back);
+			System.out.println("What is username? "+username);
+			String q= request.getQueryString();
+			System.out.println(q);
+			System.out.println("OMG redirects work! :O");
+			
+			
+				
+				//if(request.authenticate(response)){
+						request.getSession().setAttribute("Authorized", "Success");
+						request.getSession().setAttribute("user", username);
+				//}
+				
+				
+				
 		}
 		else {
 			this.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
 		}
+					
+	}
+	
 				
 			
 
@@ -63,56 +108,19 @@ public class Admin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		
-		if (request.getParameter("login") != null)
-		{
-			
-				String hash = "emptyHash";
-				try
-				{
-					hash = javax.xml.bind.DatatypeConverter.printHexBinary(MessageDigest.getInstance("SHA1").digest((request.getParameter("pass")).getBytes()));
-					//hash = request.getParameter("pass");
-					System.out.print(hash);
-				} catch (NoSuchAlgorithmException e)
-				{
-					e.printStackTrace();
-				}
-				
-				String realPath = request.getServletContext().getRealPath("/WEB-INF/passwords");
-				
-				BufferedReader file = new BufferedReader(new FileReader(realPath));
-				
-				String line = file.readLine();
-				
-				while (line != null)
-				{
-					String[] split = line.split(" ");
-					String login = split[0];
-					String password = split[1];
-					
-					if (login.equals(request.getParameter("name")) && password.equals(hash))
-					{
-						request.getSession().setAttribute("Authorized", "Success");
-						request.getSession().setAttribute("user", request.getParameter("name"));
-						break;
-					}	
-					line = file.readLine();
-				}
-				
-		}
-		
+		doGet(request, response);
+	/*	
 		if (request.getSession().getAttribute("Authorized") != null)
 		{
-		//	this.getServletContext().getRequestDispatcher("/Account.jsp").forward(request, response);
-
+			
 			if (request.getParameter("logout") !=null){
 				request.getSession().removeAttribute("Authorized");
+				request.getSession().invalidate();
 				this.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
 
 			}
 			else if(request.getParameter("browse") !=null){
-				this.getServletContext().getRequestDispatcher("/Catalogue.jsp").forward(request, response);
-
+				this.getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
 			}
 			else {
 			this.getServletContext().getRequestDispatcher("/Account.jsp").forward(request, response);
@@ -120,9 +128,45 @@ public class Admin extends HttpServlet {
 		}
 		else
 		{
-			this.getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
-			
+			if (request.getParameter("login") != null)
+			{
+				
+					String hash = "emptyHash";
+					try
+					{
+						hash = javax.xml.bind.DatatypeConverter.printHexBinary(MessageDigest.getInstance("SHA1").digest((request.getParameter("pass")).getBytes()));
+						//hash = request.getParameter("pass");
+						System.out.print(hash);
+					} catch (NoSuchAlgorithmException e)
+					{
+						e.printStackTrace();
+					}
+					
+					String realPath = request.getServletContext().getRealPath("/WEB-INF/passwords");
+					
+					BufferedReader file = new BufferedReader(new FileReader(realPath));
+					
+					String line = file.readLine();
+					
+					while (line != null)
+					{
+						String[] split = line.split(" ");
+						String login = split[0];
+						String password = split[1];
+						
+						if (login.equals(request.getParameter("name")) && password.equals(hash))
+						{
+							request.getSession().setAttribute("Authorized", "Success");
+							request.getSession().setAttribute("user", request.getParameter("name"));
+							break;
+						}	
+						line = file.readLine();
+					}
+					
+			}
+						
 		}
+		*/
 	
 	}
 
